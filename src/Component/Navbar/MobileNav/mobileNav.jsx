991ef1a2f5af2ce6efe22ee1946d2ed1e81b9396
T1st <trientrung102@gmail.com>
2024-08-14 15:48:41 +0700

    ....

--- src/Component/Navbar/MobileNav/mobileNav.jsx
+++ src/Component/Navbar/MobileNav/mobileNav.jsx
@@ -1,48 +1,48 @@
 import "./mobileNav.css";
 
 const mobileNav = ({ isOpen, toggleMenu }) => {
-  const logo = "src/assets/images/logo.png";
-  const handleDownload = () => {
-    console.log("Downloading...");
-  };
-  return (
-    <>
-      <div
-        className={`mobile-menu ${isOpen ? "active" : ""}`}
-        onClick={toggleMenu}
-      >
-        <div className="mobile-menu-container">
-          <img className="logo" src={logo} alt="logo" />
-          <ul>
-            <li>
-              <a className="menu-item" href="#">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="menu-item" href="#">
-                Skills
-              </a>
-            </li>
-            <li>
-              <a className="menu-item" href="#">
-                Projects
-              </a>
-            </li>
-            <li>
-              <a className="menu-item" href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <br />
-          <button className="contact-btn" onClick={handleDownload}>
-            Hire Me
-          </button>
-        </div>
-      </div>
-    </>
-  );
+    const logo = "./assets/images/logo.png";
+    const handleDownload = () => {
+        console.log("Downloading...");
+    };
+    return (
+        <>
+            <div
+                className={`mobile-menu ${isOpen ? "active" : ""}`}
+                onClick={toggleMenu}
+            >
+                <div className="mobile-menu-container">
+                    <img className="logo" src={logo} alt="logo" />
+                    <ul>
+                        <li>
+                            <a className="menu-item" href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a className="menu-item" href="#">
+                                Skills
+                            </a>
+                        </li>
+                        <li>
+                            <a className="menu-item" href="#">
+                                Projects
+                            </a>
+                        </li>
+                        <li>
+                            <a className="menu-item" href="#">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                    <br />
+                    <button className="contact-btn" onClick={handleDownload}>
+                        Hire Me
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default mobileNav;
